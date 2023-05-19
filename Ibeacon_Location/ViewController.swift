197f031d5eb187ec2ef40e574fa0168787acb058
Sudhar San <34421911+SudharsanK58@users.noreply.github.com>
2023-05-19 19:27:48 +0530

    Swift (Scanned ibeacon and distance formula)
    Swift (Scanned ibeacon and distance formula)

--- Ibeacon_Location/ViewController.swift
+++ Ibeacon_Location/ViewController.swift
@@ -5,13 +5,17 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     var locationManager: CLLocationManager!
     
+    @IBOutlet weak var nValueLabel: UILabel!
     @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var rssiLabel: UILabel!
+    var nValue:Float = 1
     override func viewDidLoad() {
         super.viewDidLoad()
         
         locationManager = CLLocationManager()
         locationManager.delegate = self
         locationManager.requestAlwaysAuthorization()
+        
     }
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
@@ -24,6 +28,10 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         }
     }
     
+    @IBAction func nValueSlider(_ sender: UISlider) {
+        nValue = sender.value
+        nValueLabel.text = "n value : \(Int(nValue))"
+    }
     func startScanning() {
         let uuid = UUID(uuidString: "88b78a0c-34ae-44d0-b30c-84153fec0f9a")!
         let beaconRegion = CLBeaconRegion(uuid: uuid, major: 100, minor: 37, identifier: "MyBeacon")
@@ -37,11 +45,14 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             let TxPower: Double = -61 // Fixed TxPower value for your beacon
             let RSSI: Double = Double(beacon.rssi)
             var printLabel: String = ""
-            let distance = pow(10.0, (TxPower - RSSI) / 20.0)
-            let formattedDistance = String(format: "%.2f", distance)
-            printLabel = "Distance: \(formattedDistance) meters"
+            let distance = pow(10.0, (TxPower - RSSI) / Double((Int(nValue))))
+            let distanceInFeet = distance * 3.28084
+            let formattedDistance = String(format: "%.2f", distanceInFeet)
+            printLabel = "Distance: \(formattedDistance) feet"
             print(printLabel)
+            print(nValue)
             distanceLabel.text = printLabel
+            rssiLabel.text = "Rssi: \(RSSI) db"
         }
     }
 
