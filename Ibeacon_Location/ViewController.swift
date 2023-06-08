55aba0bf3d7aec2ecd3412b7e98cb60ca6a29536
Sudhar San <34421911+SudharsanK58@users.noreply.github.com>
2023-06-08 14:14:41 +0530

    Nearby BLE and Lat/Long address

--- Ibeacon_Location/ViewController.swift
+++ Ibeacon_Location/ViewController.swift
@@ -1,43 +1,27 @@
 import UIKit
 import CoreLocation
+import CoreBluetooth
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
-    
+class ViewController: UIViewController, CLLocationManagerDelegate, CBCentralManagerDelegate {
+
     var locationManager: CLLocationManager!
-    
+    var centralManager: CBCentralManager!
+
     @IBOutlet weak var nValueLabel: UILabel!
     @IBOutlet weak var distanceLabel: UILabel!
     @IBOutlet weak var rssiLabel: UILabel!
-    var nValue:Float = 1
-    let test = KalmanFilter(R: 0.008, Q: 0.1)
-
-    // Test data
-    let testData: [Float] = [-66, -64, -63, -63, -63, -66, -65, -67, -58, -60, -61, -59, -60, -63, -65, -67, -66, -68, -69, -68, -69, -70, -72, -70, -71, -70, -72, -71, -73, -72]
-
-    // Arrays to store the data for plotting
-    var x_values: [Float] = []
-    var filtered_values: [Float] = []
+    var nValue: Float = 1
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         locationManager = CLLocationManager()
         locationManager.delegate = self
         locationManager.requestAlwaysAuthorization()
-//        for x in testData {
-//            let filtered_x = test.filter(measurement: x)
-//
-//            // Append the values to the respective arrays
-//            x_values.append(x)
-//            filtered_values.append(filtered_x)
-//
-//            // Print the data and filtered data
-//            print("Data:", x)
-//            print("Filtered Data:", filtered_x)
-//        }
-        
+
+        centralManager = CBCentralManager(delegate: self, queue: nil)
     }
-    
+
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         if status == .authorizedAlways {
             if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
@@ -47,70 +31,36 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             }
         }
     }
-    
+
     @IBAction func nValueSlider(_ sender: UISlider) {
         nValue = sender.value
         nValueLabel.text = "n value : \(Int(nValue))"
     }
-    func startScanning() {
-        let uuid = UUID(uuidString: "88b78a0c-34ae-44d0-b30c-84153fec0f9a")!
-        let beaconRegion = CLBeaconRegion(uuid: uuid, major: 100, minor: 37, identifier: "MyBeacon")
 
-        locationManager.startMonitoring(for: beaconRegion)
-        locationManager.startRangingBeacons(satisfying: beaconRegion.beaconIdentityConstraint)
+    func startScanning() {
+        centralManager.scanForPeripherals(withServices: nil, options: nil)
     }
 
-    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
-        if let beacon = beacons.first {
-            let TxPower: Double = -61 // Fixed TxPower value for your beacon
-            let RSSI: Double = Double(beacon.rssi)
-            let filtered_rssi = test.filter(measurement: Float(RSSI))
-            x_values.append(Float(RSSI))
-            filtered_values.append(filtered_rssi)
-            rssiLabel.text = "Rssi: \(RSSI) db"
-//            var printLabel: String = ""
-//            let distance = pow(10.0, (TxPower - RSSI) / Double((Int(nValue))))
-//            let distanceInFeet = distance * 3.28084
-//            let formattedDistance = String(format: "%.2f", distanceInFeet)
-//            printLabel = "Filtered Rssi: \(formattedDistance) db"
-//            print(printLabel)
-//            print(nValue)
-
-            
-            if filtered_values.count >= 10 && filtered_values.count % 10 == 0 {
-                let average_rssi = filtered_values.suffix(10).reduce(0, +) / 10.0
-                let formattedDistance = String(format: "%.2f", average_rssi)
-                let printLabel = "Filtered RSSI: \(formattedDistance) db"
-                distanceLabel.text = printLabel
-                print(filtered_values)
-            }
-            
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn {
+            startScanning()
+        } else {
+            // Handle Bluetooth not available or powered off state
         }
     }
 
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String: Any], rssi RSSI: NSNumber) {
+        if let deviceName = advertisementData[CBAdvertisementDataLocalNameKey] as? String {
+            let peripheralIdentifier = peripheral.identifier
+            print("\(deviceName)------\(peripheralIdentifier)")
+        }
 
-    
-//    func updateDistance(_ distance: CLProximity) {
-//        UIView.animate(withDuration: 0.8) {
-//            switch distance {
-//                case .unknown:
-//                    self.view.backgroundColor = UIColor.gray
-//
-//                case .far:
-//                    self.view.backgroundColor = UIColor.blue
-//
-//                case .near:
-//                    self.view.backgroundColor = UIColor.orange
-//
-//                case .immediate:
-//                    self.view.backgroundColor = UIColor.red
-//
-//                @unknown default:
-//                    // Handle any future unknown values
-//                    self.view.backgroundColor = UIColor.black
-//            }
-//
-//        }
-//    }
-    
+        // Get current location information
+        locationManager.startUpdatingLocation()
+        if let location = locationManager.location {
+            let latitude = location.coordinate.latitude
+            let longitude = location.coordinate.longitude
+            print("Latitude: \(latitude), Longitude: \(longitude)")
+        }
+    }
 }
