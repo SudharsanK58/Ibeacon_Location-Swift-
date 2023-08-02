bdf2ba68ad7fce3663a3dfaf2e920c32b78ff3a8
Sudhar San <34421911+SudharsanK58@users.noreply.github.com>
2023-08-02 16:56:47 +0530

    Correct hex code format

--- Ibeacon_Location/ViewController.swift
+++ Ibeacon_Location/ViewController.swift
@@ -4,10 +4,12 @@ import CoreBluetooth
 
 class ViewController: UIViewController, CLLocationManagerDelegate, CBCentralManagerDelegate, CBPeripheralDelegate {
     var hexRecords: String?
+    var storedLines: [String] = []
     var locationManager: CLLocationManager!
     var centralManager: CBCentralManager!
     var targetPeripheral: CBPeripheral?
-
+    var responseString: String?
+    
     @IBOutlet weak var nValueLabel: UILabel!
     @IBOutlet weak var distanceLabel: UILabel!
     @IBOutlet weak var rssiLabel: UILabel!
@@ -15,10 +17,18 @@ class ViewController: UIViewController, CLLocationManagerDelegate, CBCentralMana
 
     override func viewDidLoad() {
         super.viewDidLoad()
+//        // Call the function to store the non-empty lines from the dynamic API response
+//        storeNonEmptyLinesFromDynamicResponse()
+
+        // Wait for a moment to allow API request and response
+//        sleep(4)
+
+//        // Call the function to print the stored lines with a delay of 100 milliseconds
+//        printStoredLinesOneByOne()
+////        fetchHexRecords()
         locationManager = CLLocationManager()
         locationManager.delegate = self
         locationManager.requestAlwaysAuthorization()
-
         centralManager = CBCentralManager(delegate: self, queue: nil)
     }
 
@@ -26,7 +36,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, CBCentralMana
         if status == .authorizedAlways {
             if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
                 if CLLocationManager.isRangingAvailable() {
-                    startScanning()
+                   startScanning()
                 }
             }
         }
@@ -51,9 +61,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate, CBCentralMana
 
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String: Any], rssi RSSI: NSNumber) {
         if let deviceName = advertisementData[CBAdvertisementDataLocalNameKey] as? String {
-            if deviceName == "BIBO 1.1 A" {
+            if deviceName == "QE_V2" {
                 print("Found target device: \(deviceName)")
-//                print(peripheral)
                 targetPeripheral = peripheral
                 centralManager.connect(peripheral, options: nil)
             }
@@ -75,18 +84,21 @@ class ViewController: UIViewController, CLLocationManagerDelegate, CBCentralMana
     }
 
     func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
-        guard let characteristics = service.characteristics else { return }
+            guard let characteristics = service.characteristics else { return }
 
-        for characteristic in characteristics {
-            // Replace with the UUID of the UART characteristic if known
-            if characteristic.properties.contains(.write) || characteristic.properties.contains(.writeWithoutResponse) {
-                let message = "Not working"
-                if let data = message.data(using: .utf8) {
-                    peripheral.writeValue(data, for: characteristic, type: .withResponse) // Or .withoutResponse if appropriate
+            for characteristic in characteristics {
+//                print(characteristic)
+                // Replace with the UUID of the UART characteristic if known
+                if characteristic.properties.contains(.write) || characteristic.properties.contains(.writeWithoutResponse) {
+//                    let message = ":100000004643464200000156000000000103030081\n:1000100000000000000000000000000000000000E0"
+                    let message = ":0200000460009A\r:100000004643464200000156000000000103030081\n:1000100000000000000000000000000000000000E0\n:100060000000000000000000000000000000000090\n:00000001FF"
+                    print(message)
+                    if let data = message.data(using: .utf8) {
+                        peripheral.writeValue(data, for: characteristic, type: .withResponse) // Or .withoutResponse if appropriate
+                    }
                 }
             }
         }
-    }
 
 
     func peripheral(_ peripheral: CBPeripheral, didWriteValueFor characteristic: CBCharacteristic, error: Error?) {
@@ -133,5 +145,66 @@ class ViewController: UIViewController, CLLocationManagerDelegate, CBCentralMana
         }.resume()
     }
 
+    func storeNonEmptyLinesFromDynamicResponse() {
+        // The API URL
+        let urlString = "https://iot-api.karthi.ind.in/news-api/download/frimware?url=https://karthi.ind.in/trynew.hex"
+        
+        // Create a URL from the string
+        if let url = URL(string: urlString) {
+            // Create a URLSession
+            let session = URLSession.shared
+            
+            // Create a data task with the URL
+            let task = session.dataTask(with: url) { data, response, error in
+                // Check for errors
+                if let error = error {
+                    print("Error: \(error)")
+                    return
+                }
+                
+                // Check if we received a response
+                guard let httpResponse = response as? HTTPURLResponse else {
+                    print("No response received")
+                    return
+                }
+                
+                // Check the status code of the response
+                guard (200...299).contains(httpResponse.statusCode) else {
+                    print("Invalid response: \(httpResponse.statusCode)")
+                    return
+                }
+                
+                // Check if we received any data
+                guard let responseData = data else {
+                    print("No data received")
+                    return
+                }
+                
+                // Process and print the data as a String
+                if let responseString = String(data: responseData, encoding: .utf8) {
+                    print("API Response:\n\(responseString)")
+                } else {
+                    print("Invalid data format")
+                }
+            }
+            
+            // Start the data task
+            task.resume()
+        } else {
+            print("Invalid URL")
+        }
+    }
+
+    
+    func printStoredLinesOneByOne() {
+        // Iterate through the stored lines
+        for line in storedLines {
+            // Print the line
+            print(line)
+            
+            // Add a delay of 100 milliseconds
+            Thread.sleep(forTimeInterval: 0.1)
+        }
+    }
 
 }
