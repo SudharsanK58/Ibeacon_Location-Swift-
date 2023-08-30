688f8faeb0049b65eef425f6de8b523c04a1f86f
Sudhar San <34421911+SudharsanK58@users.noreply.github.com>
2023-08-30 16:06:19 +0530

    201010#Offline#0#12345678#12-02-2022 18:00#11-09-2024 16:00 to BIBO 1.1 A

--- Ibeacon_Location/ViewController.swift
+++ Ibeacon_Location/ViewController.swift
@@ -1,60 +1,137 @@
 import UIKit
+import CoreLocation
 import CoreBluetooth
 
-class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
-    
+class ViewController: UIViewController, CLLocationManagerDelegate, CBCentralManagerDelegate, CBPeripheralDelegate {
+    var hexRecords: String?
+    var locationManager: CLLocationManager!
     var centralManager: CBCentralManager!
     var targetPeripheral: CBPeripheral?
-    var uartCharacteristic: CBCharacteristic?
-    
+
+    @IBOutlet weak var nValueLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var rssiLabel: UILabel!
+    var nValue: Float = 1
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+
         centralManager = CBCentralManager(delegate: self, queue: nil)
     }
-    
-    func centralManagerDidUpdateState(_ central: CBCentralManager) {
-        if central.state == .poweredOn {
-            centralManager.scanForPeripherals(withServices: nil, options: nil)
-        }
-    }
-    
-    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
-        if peripheral.name == "37-Device" {
-            centralManager.stopScan()
-            targetPeripheral = peripheral
-            targetPeripheral?.delegate = self
-            centralManager.connect(targetPeripheral!, options: nil)
-        }
-    }
-    
-    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
-        print("Connected to peripheral: \(peripheral.name ?? "Unknown Device")")
-        // Now that we're connected, discover services and characteristics.
-        targetPeripheral?.discoverServices(nil)
-    }
-    
-    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
-        if let services = peripheral.services {
-            for service in services {
-                // Discover characteristics for the service.
-                peripheral.discoverCharacteristics(nil, for: service)
-            }
-        }
-    }
-    
-    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
-        if let characteristics = service.characteristics {
-            for characteristic in characteristics {
-                // Check if this is the UART characteristic.
-                if characteristic.uuid == CBUUID(string: "6e400002-b5a3-f393-e0a9-e50e24dcca9e") {
-                    uartCharacteristic = characteristic // Store the UART characteristic reference.
-                    // Send the string "301" over the UART characteristic.
-                    if let data = "201001#vasanthaka#0#69207825#08-23-2023 20:31#08-23-2023 23:31".data(using: .utf8) {
-                        peripheral.writeValue(data, for: characteristic, type: .withResponse)
-                    }
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                if CLLocationManager.isRangingAvailable() {
+                    startScanning()
                 }
             }
         }
     }
+
+    @IBAction func nValueSlider(_ sender: UISlider) {
+        nValue = sender.value
+        nValueLabel.text = "n value : \(Int(nValue))"
+    }
+
+    func startScanning() {
+        centralManager.scanForPeripherals(withServices: nil, options: nil)
+    }
+
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn {
+            startScanning()
+        } else {
+            // Handle Bluetooth not available or powered off state
+        }
+    }
+
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String: Any], rssi RSSI: NSNumber) {
+        if let deviceName = advertisementData[CBAdvertisementDataLocalNameKey] as? String {
+            if deviceName == "BIBO 1.1 A" {
+                print("Found target device: \(deviceName)")
+//                print(peripheral)
+                targetPeripheral = peripheral
+                centralManager.connect(peripheral, options: nil)
+            }
+        }
+    }
+
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        print("Connected to peripheral: \(peripheral.name ?? "")")
+        targetPeripheral?.delegate = self
+        targetPeripheral?.discoverServices([CBUUID(string: "6E400001-B5A3-F393-E0A9-E50E24DCCA9E")])
+    }
+
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        guard let services = peripheral.services else { return }
+        for service in services {
+            peripheral.discoverCharacteristics([CBUUID(string: "6E400002-B5A3-F393-E0A9-E50E24DCCA9E")], for: service)
+
+        }
+    }
+
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        guard let characteristics = service.characteristics else { return }
+
+        for characteristic in characteristics {
+            // Replace with the UUID of the UART characteristic if known
+            if characteristic.properties.contains(.write) || characteristic.properties.contains(.writeWithoutResponse) {
+                let message = "201010#Offline#0#12345678#12-02-2022 18:00#11-09-2024 16:00"
+                if let data = message.data(using: .utf8) {
+                    peripheral.writeValue(data, for: characteristic, type: .withResponse) // Or .withoutResponse if appropriate
+                }
+            }
+        }
+    }
+
+
+    func peripheral(_ peripheral: CBPeripheral, didWriteValueFor characteristic: CBCharacteristic, error: Error?) {
+        if let error = error {
+            print("Error writing value to characteristic: \(error)")
+        } else {
+            print("Successfully wrote value to characteristic")
+        }
+    }
+    func sendHex(hexLine: String) -> String{
+        return hexLine
+    }
+    func printLinesWithDelay(lines: [String], currentIndex: Int) {
+        if currentIndex < lines.count {
+            let line = lines[currentIndex].trimmingCharacters(in: .whitespacesAndNewlines)
+            if !line.isEmpty {
+                print(line)
+            }
+            DispatchQueue.global(qos: .userInteractive).asyncAfter(deadline: .now() + 1) {
+                self.printLinesWithDelay(lines: lines, currentIndex: currentIndex + 1) // Call recursively with a delay of 1 second (1000ms)
+            }
+        }
+    }
+
+    func fetchHexRecords() {
+        let url = URL(string: "https://karthi.ind.in/trynew.hex")!
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("Error fetching data: \(error)")
+                return
+            }
+
+            guard let data = data, let responseString = String(data: data, encoding: .utf8) else {
+                print("No data received")
+                return
+            }
+
+            // Remove empty lines and whitespace characters from the response string
+            let filteredResponse = responseString.replacingOccurrences(of: "\n", with: "").replacingOccurrences(of: " ", with: "")
+
+            // Print the response line by line with a delay of 1 second
+            let lines = filteredResponse.components(separatedBy: ":")
+            self.printLinesWithDelay(lines: lines, currentIndex: 0)
+        }.resume()
+    }
+
+
 }
